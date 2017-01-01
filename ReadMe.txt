f40b29bc70f0af2660812ae458c64c3d792be1d8
SubrahmanyaGanapumane <subrahmanyagg@gmail.com>
2017-01-01 09:21:14 +0530

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+test class in one package which contained protected method.
+Assignment4.1 is main method in onother package. which import test class.
---
+++ Test.java
@@ -0,0 +1,13 @@
+package session4_2;
+
+public class Test {
+	private int i=10;
+	
+	protected int Mymethod ()
+	{
+		System.out.println("In Protected method.");
+		return i;
+		
+	}
+
+}
